93f10691220b9a0606b92114fa9edcf885e693c2
konarlook <usikov.dv@gmail.com>
2024-02-29 22:36:08 +0300

    fix(06_api):add full API

--- auth/src/api/v1/users.py
+++ auth/src/api/v1/users.py
@@ -2,41 +2,28 @@ from fastapi import APIRouter, Depends, status
 from fastapi.security import HTTPBasic
 from redis.asyncio import Redis
 
-from schemas.users import (CreateUserSchema,
-                           UserBaseSchema,
-                           LoginUserResponseSchema,
-                           LoginUserSchema,
-                           LoginUserResponseSchema,
-                           MainInfoUserSchema,
-                           RefreshTokenUserSchema,
-                           ChangePasswordSchema)
+from schemas import users
 from services.user_service import AuthUserService, get_user_service
 from helpers.exceptions import AuthException
-from helpers.password import (verify_password,
-                              create_token,
-                              create_refresh_token,
-                              check_refresh_token,
-                              create_tokens,
-                              delete_refresh_token,
-                              decode_token)
+from helpers import password
 from db.redis import get_redis
 
-router = APIRouter(prefix='/auth', tags=['Auth', ])
+router = APIRouter(prefix='/auth', tags=['Auth'])
 security = HTTPBasic()
 
 
 @router.get(
     path="/signup/",
-    response_model=UserBaseSchema,
+    response_model=users.UserBaseSchema,
     status_code=status.HTTP_201_CREATED,
     summary='Регистрация пользователя',
     description='Регистрация пользователя по обязательным полям',
     tags=['Страница регистрации'],
 )
 async def create_user(
-        user_dto: CreateUserSchema = Depends(),
+        user_dto: users.CreateUserSchema = Depends(),
         user_service: AuthUserService = Depends(get_user_service)
-) -> UserBaseSchema:
+) -> users.UserBaseSchema:
     """User registration endpoint by required fields."""
     request_email = await user_service.get(email=user_dto.email)
     if request_email:
@@ -50,16 +37,16 @@ async def create_user(
 
 @router.get(
     path="/login/",
-    response_model=LoginUserResponseSchema,
+    response_model=users.LoginUserResponseSchema,
     summary='Авторизация пользователя',
     description='Регистрация пользователя по логину и паролю',
     tags=['Основная страница', 'Авторизация пользователя'],
 )
 async def login_user(
-        user_dto: LoginUserSchema = Depends(),
+        user_dto: users.LoginUserSchema = Depends(),
         user_service: AuthUserService = Depends(get_user_service),
         redis: Redis = Depends(get_redis)
-) -> LoginUserResponseSchema:
+) -> users.LoginUserResponseSchema:
     """User login endpoint by email and password."""
     response = await user_service.check_user(user_dto)
     if not response:
@@ -67,23 +54,23 @@ async def login_user(
             message="Incorrect email or password.",
             status_code=status.HTTP_404_NOT_FOUND,
         )
-    tokens = await create_tokens(response.id, redis)
-    return LoginUserResponseSchema(**tokens)
+    tokens = await password.create_tokens(response.id, redis)
+    return users.LoginUserResponseSchema(**tokens)
 
 
 @router.get(
     path='/refresh/',
-    response_model=LoginUserResponseSchema,
+    response_model=users.LoginUserResponseSchema,
     summary='Обновления refresh token',
     description='Получение новых access token и refresh token',
     tags=['Обновление токена'],
 )
 async def refresh_token(
-        refresh_info: RefreshTokenUserSchema = Depends(),
+        refresh_info: users.RefreshTokenUserSchema = Depends(),
         redis: Redis = Depends(get_redis),
-) -> LoginUserResponseSchema:
+) -> users.LoginUserResponseSchema:
     """Get new access and refresh tokens."""
-    if not await check_refresh_token(
+    if not await password.check_refresh_token(
             refresh_info.refresh_token,
             redis,
     ):
@@ -91,10 +78,10 @@ async def refresh_token(
             message="Incorrect token.",
             status_code=status.HTTP_404_NOT_FOUND,
         )
-    response = await decode_token(refresh_info.refresh_token)
-    _new_tokens = await create_tokens(response['user_id'], redis)
-    await delete_refresh_token(refresh_info.refresh_token, redis)
-    return LoginUserResponseSchema(**_new_tokens)
+    response = await password.decode_token(refresh_info.refresh_token)
+    _new_tokens = await password.create_tokens(response['user_id'], redis)
+    await password.delete_refresh_token(refresh_info.refresh_token, redis)
+    return users.LoginUserResponseSchema(**_new_tokens)
 
 
 @router.get(
@@ -104,9 +91,9 @@ async def refresh_token(
     tags=['Logout', ],
 )
 async def logout_user(
-        refresh_info: RefreshTokenUserSchema = Depends(),
+        refresh_info: users.RefreshTokenUserSchema = Depends(),
         redis: Redis = Depends(get_redis),
 ) -> dict:
     """Logout endpoint by access token."""
-    await delete_refresh_token(refresh_info.refresh_token, redis)
+    await password.delete_refresh_token(refresh_info.refresh_token, redis)
     return {'detail': 'logout is successfully'}
